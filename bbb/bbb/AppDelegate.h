9738667964f6943605492c894f607604f208f4e9
ge <ge@gedemacbook-pro.local>
2016-04-19 21:07:15 +0800

    初始化程序

---
+++ bbb/bbb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bbb
+//
+//  Created by ge on 16/4/19.
+//  Copyright © 2016年 ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bbb/bbb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bbb
+//
+//  Created by ge on 16/4/19.
+//  Copyright © 2016年 ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
