1a36fea49a6737ee5929a97105e5fef140dabe60
ge <ge@gedemacbook-pro.local>
2016-04-19 21:04:49 +0800

    初始化项目

---
+++ aaaa/aaaa/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  aaaa
+//
+//  Created by ge on 16/4/19.
+//  Copyright © 2016年 ge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
